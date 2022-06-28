ab2afd37f79a952fb09f899b16ed41c54c7804dc
ribsey <lukas.ribi@gmail.com>
2022-06-28 14:38:35 +0200

    updated script

--- fun_stuff/AdSi/adsi.py
+++ fun_stuff/AdSi/adsi.py
@@ -1,17 +1,20 @@
-from mailbox import mbox
+import json
 import os
 import ctypes
 from random import randint, random
 from time import sleep
+from urllib.request import urlretrieve, urlopen
+import tempfile
 
-LINKS = ["https://fakeupdate.net/win10ue/",
-         "https://fakeupdate.net/win10/", "https://fakeupdate.net/wnc/", "https://www.omfgdogs.com/#"]
+# LINKS = ["https://fakeupdate.net/win10ue/",
+#          "https://fakeupdate.net/win10/", "https://fakeupdate.net/wnc/", "https://www.omfgdogs.com/#"]
+LINKS = ["%TEMP%\\Rick_Astley_-_Never_Gonna_Give_You_Up.mp4"]
 
 GIFS = {
     "really": "https://c.tenor.com/r7OYRTWn1C0AAAAC/kevin-hart-stare.gif"
 }
 
-FIREFOX = '"\\Program Files\\Mozilla Firefox\\firefox.exe" --kiosk --new-window '
+FIREFOX = '"C:\\Program Files\\Mozilla Firefox\\firefox.exe" --kiosk --new-window '
 
 MESSAGEBOXES = [18, 20]  # , 22, 36, 48, 51, 52]
 
@@ -88,21 +91,41 @@ def messageBoxHandler(style):
     #     button = Mbox("", "", style)
 
 
+def prepRickTroll():
+    file_path = os.path.realpath(__file__)
+    rickPath = os.path.dirname(file_path) + \
+        "\\Rick_Astley_-_Never_Gonna_Give_You_Up.mp4"
+
+    os.popen(f'copy {rickPath} "%TEMP%"')
+
+
 def randomHandler(nr):
     if nr >= 0 and nr < len(LINKS):
         openKiosk(LINKS[nr])
     elif nr < (len(LINKS) + len(MESSAGEBOXES)):
         messageBoxHandler(MESSAGEBOXES[nr - len(LINKS)])
+    else:
+        randMeme()
 
 
 def hoursToSeconds(hours):
     return hours * 60 * 60
 
 
+def randMeme():
+    memeURL = json.loads(
+        urlopen('https://meme-api.herokuapp.com/gimme').read())['url']
+    memeFile = f'{tempfile.gettempdir()}\\meme.{memeURL.split(".")[-1]}'
+    urlretrieve(memeURL, memeFile)
+    openKiosk(memeFile)
+
+
+prepRickTroll()
+
 while(1):
     try:
         # Random run time between 1 and 2.5 hours
         sleep(hoursToSeconds(random()*1.5+1))
-        randomHandler(randint(0, len(LINKS) + len(MESSAGEBOXES) - 1))
+        randomHandler(randint(0, len(LINKS) + len(MESSAGEBOXES)))
     except KeyboardInterrupt:
         break
